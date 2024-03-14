d22fd40643480bc29fecbccbfb451a4a7bf756c1
ABSOLUTEbeginnerr <82446276+ABSOLUTEbeginnerr@users.noreply.github.com>
2024-03-14 12:05:31 +0200

    .

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 require('dotenv').config();
+const requestIp = require('request-ip');
 var express = require('express');
 var app = express();
 
@@ -18,9 +19,14 @@ app.get('/', function (req, res) {
 
 
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.use(requestIp.mw());
+
+app.get('/api/whoami', (req, res) => {
+  const ipaddress = req.clientIp;
+  const language = req.headers['accept-language'];
+  const software = req.headers['user-agent'];
+
+  res.json({ ipaddress, language, software });
 });
 
 
