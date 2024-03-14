f47b47d74b6bd454e3ea281bfbf4097f1cb5e5c9
ABSOLUTEbeginnerr <82446276+ABSOLUTEbeginnerr@users.noreply.github.com>
2024-03-14 12:01:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-// index.js
-// where your node app starts
 
-// init project
 require('dotenv').config();
 var express = require('express');
 var app = express();
@@ -19,10 +16,15 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+
+
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+
+
 
 // listen for requests :)
 var listener = app.listen(process.env['PORT']|| 3000, function () {
